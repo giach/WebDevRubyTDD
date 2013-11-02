18c4bcbfa27e125f7e277dd57e43d90b73bff00d
Georgiana Rodica Chelu <georgiana.chelu93@gmail.com>
2013-11-02 13:20:13 +0200

    completed pascal's triangle

--- PascalsTriangle/pascalstriangle.rb
+++ PascalsTriangle/pascalstriangle.rb
@@ -1,4 +1,28 @@
 def compute_row(n)
-  []
+  return [1,1] if n == 2
+  return [1] if n == 1
+  return nil unless n.is_a? Fixnum
+  
+  a = [1,1]
+  aux = []
+
+	if n > 2
+		aux.push(1)
+		(1..n-2).each do |j|
+			(0..j-1).each do |i|
+				#puts  a[i], a[i + 1]
+				sum = a[i] + a[i + 1]
+				aux.push(sum)
+			end
+			aux.push(1)
+			a = aux
+			aux = []
+			aux.push(1)
+		end
+	end
+
+	return a
 end
+
+#compute_row(5)
 
